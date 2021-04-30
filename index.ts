9da6f9df26c29edc3bf1f5807c6dfe7f8fa4070a
Sam Sheets <sam.sheets94@gmail.com>
2021-04-30 16:39:36 -0600

    checking types

--- index.ts
+++ index.ts
@@ -1,20 +1,26 @@
-const arrayExample = [1, 3, 4, 5]
+import { Bear } from './bear.model'
+
+// const arrayExample = [1, 3, 4, 5]
 //same thing:
 //const arrayExample: number[] = [1, 3, 4, 5]
 
-let example1: boolean = true;
+// let example1: boolean = true;
 
-let example2: number;
-example2 = 35;
+// let example2: number;
+// example2 = 35;
 
-let example3 = 'Hello World';
+// let example3 = 'Hello World';
 //same thing:
 //let example3: string = 'Hello World';
 
 //multiple types
-let example4: boolean | number = 35;
+// let example4: boolean | number = 35;
 
-let example5: undefined = undefined;
+// let example5: undefined = undefined;
 
-let example6: null = null
+// let example6: null = null
 
+const bear = new Bear(3)
+if(bear instanceof Bear) {
+    console.log('Hwllo from TypeScript')
+}
