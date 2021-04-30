a611142b55007432a85a31c5efcee11c78afff13
Sam Sheets <sam.sheets94@gmail.com>
2021-04-30 16:33:48 -0600

    implicit vs explicit types

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
+const arrayExample = [1, 3, 4, 5]
+//same thing:
+//const arrayExample: number[] = [1, 3, 4, 5]
+
 let example1: boolean = true;
 
-let example2: number = 35;
+let example2: number;
+example2 = 35;
 
-let example3: string = 'Hello World';
+let example3 = 'Hello World';
+//same thing:
+//let example3: string = 'Hello World';
 
+//multiple types
 let example4: boolean | number = 35;
+
+let example5: undefined = undefined;
+
+let example6: null = null
+
