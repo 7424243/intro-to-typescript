85f301a25c0becc9475ae02f7e748af7aa49bba3
Sam Sheets <sam.sheets94@gmail.com>
2021-05-01 14:00:35 -0600

    parameters

--- index.ts
+++ index.ts
@@ -49,3 +49,17 @@ const example7: object = undefined
 
 //an empty object:
 const example8: {} = {}
+
+/* Parameters */
+import {Person} from './person.model'
+function add(val1: number, val2: number) {
+    return val1 + val2
+}
+add(1, 3)
+
+function sayHello(person: Person) {
+    return `Say Hello to My Little Friend, ${person.firstName}`
+}
+sayHello(new Person({firstName: 'Dylan'}))
+
+
